66e1ded67aa3c91944d72f0543e35be4d514613c
Adamina Flores <adaminaflores19@gmail.com>
2021-11-15 11:06:12 -0600

    Finished JUnit exercise

---
+++ src/test/java/TestingWithJunit.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestingWithJunit {
+
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello World");
+
+    }
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+    @Test
+    public void testIfObjectsAreDifferent() {
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonedSheep = sheep;
+
+        assertNotSame(sheep, dog);
+        assertSame(sheep, clonedSheep);
+    }
+}
